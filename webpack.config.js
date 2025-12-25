598986a0428dd4d40d90d6a59d578e6e3505520a
Satyam Chourey <satyamchourey26@gmail.com>
2025-12-24 19:41:00 -0500

    Github page setup done

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: process.env.NODE_ENV === 'production' ? '/tentwenty/' : '/',
     clean: true,
   },
   resolve: {
